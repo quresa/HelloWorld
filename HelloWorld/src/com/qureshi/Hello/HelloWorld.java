84f3071c87f9f37da5101f26d01231dab3685f22
quresa <ayaaz.qureshi@gmail.com>
2015-06-20 18:05:15 +0800

    First commit

---
+++ HelloWorld/src/com/qureshi/Hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.qureshi.Hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println(new Message1().toString());
+	}
+}
---
+++ HelloWorld/src/com/qureshi/Hello/Message1.java
@@ -0,0 +1,8 @@
+package com.qureshi.Hello;
+
+public class Message1 {
+	@Override
+	public String toString() {
+		return "Hello bitches";
+	}
+}
