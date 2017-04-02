50d4b8cee279e520c0e0214825732cc709cfd359
quresa <ayaaz.qureshi@gmail.com>
2017-04-02 22:07:01 +0800

    Third message

--- HelloWorld/src/com/qureshi/Hello/HelloWorld.java
+++ HelloWorld/src/com/qureshi/Hello/HelloWorld.java
@@ -3,6 +3,6 @@ package com.qureshi.Hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println(new Message2().toString());
+		System.out.println(new Message3().toString());
 	}
 }
